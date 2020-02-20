27217eec95e523daa4d7abbf549329fe72153591
Jorge Sá Pereira <me@jorgesapereira.com>
2020-02-20 16:51:50 +0000

    Initial commit

---
+++ .build/build.php
@@ -0,0 +1,128 @@
+<?php
+
+use Composer\Semver\Comparator;
+use GitWrapper\GitCommand;
+use GitWrapper\GitWrapper;
+use GitWrapper\Event\GitLoggerEventSubscriber;
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+require 'vendor/autoload.php';
+
+$url = "https://api.github.com/repos/conversejs/converse.js/releases";
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_HEADER, 0);
+curl_setopt($ch, CURLOPT_USERAGENT, "Awesome-Octocat-App");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+$response = curl_exec($ch);
+curl_close($ch);
+
+$since = 'v5.0.0';
+
+$data = json_decode($response, true);
+
+$parsedData = [];
+$sourceTags = [];
+
+foreach($data as $release) {
+
+  $tag = $release['tag_name'];
+
+  // To include v prefix if missing like (3.3.2)
+  if (is_numeric($tag{0})) {
+    $tag = 'v'.$tag;
+  }
+
+  if (Comparator::lessThan($tag, $since)) {
+    continue;
+  }
+
+  if (!isset($release['assets'][0]['browser_download_url'])) {
+      continue;
+  }
+
+  $releaseData = [
+    'tag_name' => $release['tag_name'],
+    'download_url' => $release['assets'][0]['browser_download_url']
+  ];
+
+  $sourceTags[] = $release['tag_name'];
+
+  $time = strtotime($release['published_at']);
+
+  $parsedData[$time] = $releaseData;
+}
+
+ksort($parsedData);
+
+// Log to a file named "git.log"
+$log = new Logger('git');
+$log->pushHandler(new StreamHandler('git.log', Logger::DEBUG));
+
+$sourceFolder = __DIR__ . '/tmp/source';
+$targetFolder = dirname(__DIR__);
+
+// TARGET
+$gitTargetWrapper = new GitWrapper();
+$gitTargetWrapper->addLoggerEventSubscriber(new GitLoggerEventSubscriber($log));
+
+$gitTargetRepo = $gitTargetWrapper->workingCopy($targetFolder);
+
+$listTagsCommand = new GitCommand('tag', '-l', '--sort=version:refname', 'v*');
+
+$targetTags = $gitTargetWrapper->run($listTagsCommand, $gitTargetRepo->getDirectory());
+$targetTags = explode(PHP_EOL, $targetTags);
+
+$file = 'conversejs.tgz';
+
+foreach ($parsedData as $release) {
+
+    if (file_exists($file)) {
+      unlink($file);
+    }
+
+    if (is_dir($sourceFolder)) {
+      `rm -rf $sourceFolder`;
+    }
+
+    mkdir($sourceFolder, 0777, true);
+
+    $version = $release['tag_name'];
+
+    if (in_array($version, $targetTags)) {
+      continue;
+    }
+
+    if (Comparator::lessThan($version, $since)) {
+      continue;
+    }
+
+    echo 'Detected ' . $version . PHP_EOL;
+
+    $wget = sprintf('wget %s -O %s', $release['download_url'], $file);
+    `$wget`;
+
+    $unpack = sprintf('tar -xf %s -C %s', $file, $sourceFolder);
+    `$unpack`;
+
+    // Rsync repositories
+    $rsync = sprintf(
+        "rsync -aL --delete '%s' '%s' 2>&1",
+        $sourceFolder . '/package/dist',
+        $gitTargetRepo->getDirectory()
+    );
+    `$rsync`;
+
+    // Add all changes (if file was to be ignored with .gitignore it should not be versioned in source repo)
+    $command = new GitCommand('add', 'dist');
+    $gitTargetWrapper->run($command, $gitTargetRepo->getDirectory());
+
+    // Commit
+    $gitTargetRepo->commit('Add conversejs/converse.js-dist ' . $version);
+    $gitTargetRepo->tag($version);
+
+    echo 'Added conversejs/converse.js-dist ' . $version . PHP_EOL;
+}
---
+++ README.md
@@ -0,0 +1,24 @@
+# Converse
+
+[![XMPP Chat](https://inverse.chat/badge.svg?room=discuss@conference.conversejs.org)](https://inverse.chat/#converse/room?jid=discuss@conference.conversejs.org)
+[![Travis](https://api.travis-ci.org/conversejs/converse.js.png?branch=master)](https://travis-ci.org/conversejs/converse.js)
+[![Bountysource bounties](https://img.shields.io/bountysource/team/converse.js/activity.svg?maxAge=2592000)](https://www.bountysource.com/teams/converse.js/issues?tracker_ids=194169)
+[![Translation status](https://hosted.weblate.org/widgets/conversejs/-/svg-badge.svg)](https://hosted.weblate.org/engage/conversejs/?utm_source=widget)
+
+[Converse](https://conversejs.org) is a web based [XMPP/Jabber](https://xmpp.org) chat client.
+
+You can either use it as a webchat app, or you can integrate it into your own website.
+
+It's 100% client-side JavaScript, HTML and CSS and the only backend required
+is a modern XMPP server.
+
+Please support this project via [Patreon](https://www.patreon.com/jcbrand) or [Liberapay](https://liberapay.com/jcbrand)
+
+## Notice
+
+This package can be installed using composer and ready to use.
+It includes the dist files that are available in conversejs releases.
+
+## More information
+
+[Converse.js](https://github.com/conversejs/converse.js)
